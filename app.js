7ce7d0a8c0bd093cba9c38cd934895b7e3a6fbe9
ifeoluwa <youremail@example.com>
2025-10-02 11:04:50 +0100

    added the routed for the  api and error handling

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 import express from 'express';
-import dictionary from './dictionary.json' with {type : 'json'};
-import lookupRoute from '../backend/jsonRoute.js';
+import jsonRoute from '../backend/jsonRoute.js';
+
 
 const app = express();
 //  Using Middleware
 app.use(express.json());
 app.use(express.urlencoded ({extended: true}));
 
-// accessing routes
-app.use('/jsonRoute', lookupRoute);
+// attach routes
+app.use("/api", jsonRoute);  // handles /api/define
+app.use("/api", jsonRoute);    // handles /api/add
 
 
 export default app;
